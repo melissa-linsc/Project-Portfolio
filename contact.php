1dfd34a9e7c8394e27f63850fa57ecb9ba73ad69
melissa-linsc <118235468+melissa-linsc@users.noreply.github.com>
2023-11-30 14:01:31 +0000

    formatting changes

---
+++ contact.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+First Name <?php echo $_POST["firstName"]; ?><br>
+Last Name <?php echo $_POST["surname"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?><br>
+Your message is: <?php echo $_POST["message"]; ?>
+
+</body>
+</html>
